e3640f3b8eea5c0760e6e38331c26dbdd3bfd88f
BebekCarok <keronemu@gmail.com>
2025-08-02 20:19:25 +0700

    Revert "pesan cinta dari Backend (guah) ke master (guah juga)"

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,32 @@
 <?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\ReportController;
-use App\Livewire\Dashboard;
 
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
-use App\Exports\StudentReportExport;
-use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TransactionController;
 
-Route::get('/laporan/export', function () {
-    return Excel::download(new StudentReportExport, 'laporan_siswa.xlsx');
-})->name('laporan.export');
+
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-    Route::redirect('settings', 'settings/profile');
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    
+    Route::middleware(['auth'])->group(function () {
+        Route::redirect('settings', 'settings/profile');
+        
+        Route::get('settings/profile', Profile::class)->name('settings.profile');
+        Route::get('settings/password', Password::class)->name('settings.password');
+        Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+        Route::resource('students', StudentController::class);
+        Route::resource('transactions', TransactionController::class);
 
-    Route::resource('students', StudentController::class);
-    Route::resource('transactions', TransactionController::class);
-    Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
 });
 
 require __DIR__.'/auth.php';
